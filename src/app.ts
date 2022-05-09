218362ec3fd4e4a12535db7adfa0722f61859352
1zumisawashun <shun.1zumisawa@gmail.com>
2022-05-09 23:36:13 +0900

    初回コミット

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () =>
+  console.log(`server running in http://localhost:${PORT}`)
+);
