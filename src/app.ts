5b990e23610f1945ed1b247471cb06b4114b6fc7
1zumisawashun <shun.1zumisawa@gmail.com>
2022-05-09 23:39:13 +0900

    レンダーさせる

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import path from "path";
-import express from "express";
+import express, { Request, Response } from "express";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -7,6 +8,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("index");
+});
 
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
